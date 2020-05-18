36400946a7e0f0f68a0e0480953781be8f226ffb
ShukhardinAleksandrASU19bz <65189651+ShukhardinAleksandrASU19bz@users.noreply.github.com>
2020-05-18 21:38:56 +0500

    Add files via upload

---
+++ 7 Шаблоны классов/Source.cpp
@@ -0,0 +1,43 @@
+#include "Vector.h" 
+#include <iostream>
+#include "Time.h"
+
+using namespace std; 
+void main() 
+{
+	/*
+//инициализация, ввод и вывод значений вектора 
+	Vector<int>A(5,0);
+	cin>>A; 
+	cout<<A<<endl; 
+	//инициализация и вывод значений вектора 
+	Vector <int>B(10,1);
+	cout<<B<<endl; 
+	//операция присваивания 
+	B=A; 
+	cout<<B<<endl; 
+	//доступ по индексу 
+	cout <<A[2]<<endl; 
+	//получение длины вектора 
+	cout<<"size="<<A()<<endl; 
+	//операция сложения с константой
+	B=A+10; 
+	cout<<B<<endl; 
+	*/
+	// тестирование класса Time
+	Time t;
+	cin >> t; 
+	cout << t;
+	Vector<Time>A(5, t);
+	cin >> A;
+	cout << A << endl;
+	Vector <Time>B(10, t);
+	cout << B << endl;
+	B = A;
+	cout << B << endl; 
+	cout << A[2] << endl;
+	cout << "size=" << A() << endl;
+	B = A + t; 
+	cout << B << endl;
+
+} 
---
+++ 7 Шаблоны классов/Time.h
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std; 
+
+class Time 
+{ 
+public: 
+	Time(void); 
+	Time(int, int); 
+	Time(const Time&); 
+	Time& operator=(const Time&); 
+	//операция для добавление константы
+	Time operator+(const int k);
+	//перегруженные операции ввода-вывода 
+	friend ostream& operator<< (ostream& out, const Time&); 
+	friend istream& operator>> (istream& in, Time&); 
+	Time operator+(Time k);
+public: 
+	virtual ~Time(void){}; 
+private: 
+	int min,sec; 
+};
